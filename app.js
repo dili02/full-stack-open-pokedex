9a09572c8e5d2eb298a198aa240500b0448cc338
Diego Garcia <dili02@gmail.com>
2022-01-02 04:22:27 -0300

    add depoy heroku pipeline workflow

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-console */
 const express = require("express");
 const app = express();
+const { version } = require("./package.json");
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000;
 
+app.get("/healt", (req, res) => {
+  res.send("OK");
+});
+
+app.get("/version", (req, res) => {
+  res.send(version);
+});
+
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
-  console.log("server started on port 5000");
+  console.log(`server started on port ${PORT}`);
 });
